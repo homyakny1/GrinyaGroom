b11e0ddcfa7eee4b245a137540b0878359d12ba6
Eugene Khom <homyakny@gmail.com>
2019-07-09 23:09:36 -0400

    chanhed gallery

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html, body').animate({
-                    scrollTop: (target.offset().top - 80)
+                    scrollTop: (target.offset().top - 110)
                 }, 1000, "easeInOutExpo");
                 return !1
             }
