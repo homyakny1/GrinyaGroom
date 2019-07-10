3f324f12a08c566e24c3f4a1aeda185dc53bec51
Eugene Khom <homyakny@gmail.com>
2019-07-10 00:11:38 -0400

    fixed reviews and sections'

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html, body').animate({
-                    scrollTop: (target.offset().top - 110)
+                    scrollTop: (target.offset().top - 79)
                 }, 1000, "easeInOutExpo");
                 return !1
             }
