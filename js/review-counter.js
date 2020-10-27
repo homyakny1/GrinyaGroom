2589a6025d0972249b352d7034b65eb2f0e6480e
Eugene <homyakny@gmail.com>
2020-10-26 20:57:50 -0400

    fixed logic v2

--- js/review-counter.js
+++ js/review-counter.js
@@ -1,42 +1,40 @@
+$.fn.jQuerySimpleCounter = function (options) {
+  var settings = $.extend(
+    {
+      start: 0,
+      end: 100,
+      easing: "swing",
+      duration: 400,
+      complete: "",
+    },
+    options
+  );
 
+  var thisElement = $(this);
+
+  $({ count: settings.start }).animate(
+    { count: settings.end },
+    {
+      duration: settings.duration,
+      easing: settings.easing,
+      step: function () {
+        var mathCount = Math.ceil(this.count);
+        thisElement.text(mathCount);
+      },
+      complete: settings.complete,
+    }
+  );
+};
+var i = 0;
 var waypoint = new Waypoint({
   element: document.getElementById("reviews"),
   handler: function (direction) {
-
-    console.log("Reviews are shown");
-
-    var numItems = $(".swiper-slide").length;
-
-    $.fn.jQuerySimpleCounter = function (options) {
-      var settings = $.extend(
-        {
-          start: 0,
-          end: 100,
-          easing: "swing",
-          duration: 400,
-          complete: "",
-        },
-        options
-      );
-
-      var thisElement = $(this);
-
-      $({ count: settings.start }).animate(
-        { count: settings.end },
-        {
-          duration: settings.duration,
-          easing: settings.easing,
-          step: function () {
-            var mathCount = Math.ceil(this.count);
-            thisElement.text(mathCount);
-          },
-          complete: settings.complete,
-        }
-      );
-    };
-
-    $("#revNumber").jQuerySimpleCounter({ end: numItems, duration: 3000 });
-
-    console.log(numItems+" Reviews are shown");
+    if (i == 0) {
+      console.log("Reviews are shown");
+      var numItems = $(".swiper-slide").length;
+      $("#revNumber").jQuerySimpleCounter({ end: numItems, duration: 3000 });
+      console.log(numItems + " Reviews are shown");
+      i++;
+    }
   },
 });
