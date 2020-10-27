c2dc6c35d53cb00676d05e3d636128e2f3b3edf3
Eugene <homyakny@gmail.com>
2020-10-26 20:52:46 -0400

    fixed logic

--- js/review-counter.js
+++ js/review-counter.js
@@ -1,39 +1,10 @@
-// $(window).on("load", function () {
-//   var numItems = $(".swiper-slide").length;
 
-//   $.fn.jQuerySimpleCounter = function (options) {
-//     var settings = $.extend(
-//       {
-//         start: 0,
-//         end: 100,
-//         easing: "swing",
-//         duration: 400,
-//         complete: "",
-//       },
-//       options
-//     );
-
-//     var thisElement = $(this);
-
-//     $({ count: settings.start }).animate(
-//       { count: settings.end },
-//       {
-//         duration: settings.duration,
-//         easing: settings.easing,
-//         step: function () {
-//           var mathCount = Math.ceil(this.count);
-//           thisElement.text(mathCount);
-//         },
-//         complete: settings.complete,
-//       }
-//     );
-//   };
-
-//   $("#revNumber").jQuerySimpleCounter({ end: numItems, duration: 3000 });
-// });
 var waypoint = new Waypoint({
   element: document.getElementById("reviews"),
   handler: function (direction) {
+
+    console.log("Reviews are shown");
+
     var numItems = $(".swiper-slide").length;
 
     $.fn.jQuerySimpleCounter = function (options) {
@@ -65,5 +36,7 @@ var waypoint = new Waypoint({
     };
 
     $("#revNumber").jQuerySimpleCounter({ end: numItems, duration: 3000 });
+
+    console.log(numItems+" Reviews are shown");
   },
 });
