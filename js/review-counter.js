74744237ca45328613122732836d594175213609
Eugene <homyakny@gmail.com>
2020-10-26 20:50:11 -0400

    added logic

--- js/review-counter.js
+++ js/review-counter.js
@@ -1,33 +1,69 @@
-$(window).on("load", function () {
-  var numItems = $(".swiper-slide").length;
+// $(window).on("load", function () {
+//   var numItems = $(".swiper-slide").length;
 
-  $.fn.jQuerySimpleCounter = function (options) {
-    var settings = $.extend(
-      {
-        start: 0,
-        end: 100,
-        easing: "swing",
-        duration: 400,
-        complete: "",
-      },
-      options
-    );
+//   $.fn.jQuerySimpleCounter = function (options) {
+//     var settings = $.extend(
+//       {
+//         start: 0,
+//         end: 100,
+//         easing: "swing",
+//         duration: 400,
+//         complete: "",
+//       },
+//       options
+//     );
 
-    var thisElement = $(this);
+//     var thisElement = $(this);
 
-    $({ count: settings.start }).animate(
-      { count: settings.end },
-      {
-        duration: settings.duration,
-        easing: settings.easing,
-        step: function () {
-          var mathCount = Math.ceil(this.count);
-          thisElement.text(mathCount);
+//     $({ count: settings.start }).animate(
+//       { count: settings.end },
+//       {
+//         duration: settings.duration,
+//         easing: settings.easing,
+//         step: function () {
+//           var mathCount = Math.ceil(this.count);
+//           thisElement.text(mathCount);
+//         },
+//         complete: settings.complete,
+//       }
+//     );
+//   };
+
+//   $("#revNumber").jQuerySimpleCounter({ end: numItems, duration: 3000 });
+// });
+var waypoint = new Waypoint({
+  element: document.getElementById("reviews"),
+  handler: function (direction) {
+    var numItems = $(".swiper-slide").length;
+
+    $.fn.jQuerySimpleCounter = function (options) {
+      var settings = $.extend(
+        {
+          start: 0,
+          end: 100,
+          easing: "swing",
+          duration: 400,
+          complete: "",
         },
-        complete: settings.complete,
-      }
-    );
-  };
+        options
+      );
 
-  $("#revNumber").jQuerySimpleCounter({ end: numItems, duration: 3000 });
+      var thisElement = $(this);
+
+      $({ count: settings.start }).animate(
+        { count: settings.end },
+        {
+          duration: settings.duration,
+          easing: settings.easing,
+          step: function () {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount);
+          },
+          complete: settings.complete,
+        }
+      );
+    };
+
+    $("#revNumber").jQuerySimpleCounter({ end: numItems, duration: 3000 });
+  },
 });
